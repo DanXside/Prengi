45376865396e7ec8e1724b018ca8647f345a39ec
Daniil <danya.moiseev@inbox.ru>
2021-11-02 16:09:30 +1000

    final

--- src/js/script.js
+++ src/js/script.js
@@ -9,3 +9,25 @@ new Swiper('.swiper', {
         stopOnLastSlide: true,
     }
 });
+
+const hamburger = document.querySelector('.hamburger'),
+      menu = document.querySelector('.promo__items'),
+      overlay = document.querySelector('.promo__overlay'),
+      link = document.querySelectorAll('.promo__item');
+
+hamburger.addEventListener('click', () => {
+    menu.classList.add('active');
+    overlay.classList.add('active');
+});
+
+link.forEach((e) => {
+    e.addEventListener('click', () => {
+        menu.classList.remove('active');
+        overlay.classList.remove('active');
+    });
+});
+
+overlay.addEventListener('click', () => {
+    menu.classList.remove('active');
+    overlay.classList.remove('active');
+});
